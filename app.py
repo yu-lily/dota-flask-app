0c4e9c421df6577e9b6b88c62d9017f0af307207
suzu <yalu491@gmail.com>
2022-01-11 22:53:30 -0800

    resolve databse locks

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 from dotenv import load_dotenv
 import os
@@ -10,8 +9,11 @@ from sql_client import SQLClient
 app = Flask(__name__)
 db = SQLClient()
 cur = db.cur
+conn = db.conn
 
 @app.route("/")
 def table():
-    cur.execute("SELECT * FROM abilityagg LIMIT 10;")
+    cur.execute("SELECT * FROM abilityagg LIMIT 100;")
+    conn.commit()
     return render_template("table.html", cols=cur.description, rows=cur.fetchall())
+    
